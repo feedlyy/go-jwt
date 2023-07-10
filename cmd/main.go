f65879a0c8076d75dd65ee696636c651770d66ef
Muhammad Nur Fadli <muhammad.fadli@lemonilo.com>
2023-07-10 21:51:32 +0700

    main, db setup, go mod

---
+++ cmd/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+)
+
+func init() {
+	viper.SetConfigFile(`../config/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	// Setup Logging
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	customFormatter.FullTimestamp = true
+	logrus.SetFormatter(customFormatter)
+
+	dbHost := viper.GetString(`database.host`)
+	dbUser := viper.GetString(`database.user`)
+	dbName := viper.GetString(`database.name`)
+	dbPort := viper.GetString(`database.port`)
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		dbHost, dbUser, dbName, dbPort)
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Test ping
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	logrus.Info("Successfully ping db")
+
+	// repos
+
+	// services
+
+	// handlers
+	portAddress := viper.GetString(`server.address`)
+	router := httprouter.New()
+	log.Fatal(http.ListenAndServe(portAddress, router))
+}
